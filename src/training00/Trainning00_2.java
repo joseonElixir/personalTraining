c5adc11e9a99283593bd1eb623c609298412f293
joseonElixir <blockcts123@naver.com>
2022-01-06 18:22:28 +0900

    코드 수정, 주석작업

--- src/training00/Trainning00_2.java
+++ src/training00/Trainning00_2.java
@@ -11,13 +11,14 @@ public class Trainning00_2 {
 		}
 		
 		
-		
+		//2중for문 1개로 배열에 난수생성, 중복검사, 전방 짝수, 후방 홀수, 내림차순 정렬하기.
+		//break로 쓸데없는 부분 빠져나오기.
 		for(int i = 0 ; i<arr.length;i++) {
 			arr[i] = (int) (Math.random() * range + start);
 			for(int j=0;j<i;j++) {
 				if (arr[i] == arr[j]) {
 					i--;
-				
+					break;
 				}else if((arr[i]%2!=0 && arr[j]%2!=0&& arr[j]<arr[i])||
 					(arr[i]%2==0&&arr[j]%2!=0)||
 					(arr[i]%2==0&& arr[j]%2==0&& arr[j]<arr[i])){
